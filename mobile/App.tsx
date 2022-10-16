a73354fe5249bb0f126361d9f0c9fa2b12e27d67
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-10-15 21:15:49 -0300

    Criando ambiente mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: String
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello React Native !!</Text>
+
+      <Button title='Botão 1' />
+      <Button title='Botão 2' />
+      <Button title='Botão 3' />
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
