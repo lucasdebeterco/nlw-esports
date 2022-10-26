3a7e1e1813b9e106c84db804a63735778afc5fdd
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-10-25 23:39:15 -0300

    Criando rotas iniciais

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,16 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json(['']);
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json(['']);
+})
+
+app.get('/games/:id/ads', (request, response) => {
     return response.json([
         { id: 1, name: 'Anúncio 1' },
         { id: 2, name: 'Anúncio 2' },
@@ -11,5 +19,9 @@ app.get('/ads', (request, response) => {
         { id: 4, name: 'Anúncio 5' }
     ])
 })
+
+app.get('/ads/:id/discord', (request, response) => {
+    return response.json([]);
+});
 
 app.listen(3333)
